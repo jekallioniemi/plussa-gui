6c2c3a1be74c174abf498bd198ed8d57309f9d9a
jkaartoluoma <joonas.kaartoluoma@gmail.com>
2019-03-01 16:58:13 +0200

    simple http get

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+
   title = 'plussa-gui';
+  response: any;
+
+  constructor(
+    private http:HttpClient
+  ){}
+
+  ngOnInit(){
+    this.get();
+  }
+
+  get(){
+
+    // GitLab private token
+    var token = "zhTide4FLViFeUXgZf_D";
+    // GitLab API URL split into variables
+    var apiUrl = "https://gitlab.com/api/v4/projects/";
+    var projectId = "11077015";
+    var filePath = "/repository/files/testi.txt?ref=master";
+
+    const headers = new HttpHeaders().set("PRIVATE-TOKEN", token);
+    this.http.get(apiUrl + projectId + filePath, {headers})
+    .subscribe((response) => {
+      this.response = response;
+      console.log(this.response);
+    });
+  }
+
 }
