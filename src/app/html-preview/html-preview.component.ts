4d584b7d75250a4f27ab10af982fe77b13abb200
pjarvinen <petteri.v.jarvinen@tuni.fi>
2019-03-12 11:30:26 +0200

    Initial branch commit

---
+++ src/app/html-preview/html-preview.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-html-preview',
+  templateUrl: './html-preview.component.html',
+  styleUrls: ['./html-preview.component.css']
+})
+export class HtmlPreviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/html-preview/html-preview.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HtmlPreviewComponent } from './html-preview/html-preview.component';
+
+@NgModule({
+  declarations: [HtmlPreviewComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class HtmlPreviewModule { }
