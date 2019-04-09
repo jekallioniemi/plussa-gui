233e8cd6d0a20ce4eaadee1431b07461f3777089
jekallioniemi <jere.kallioniemi@tuni.fi>
2019-04-09 14:08:54 +0300

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-
+import { Component, NgModule, Input, ComponentFactory, ComponentRef, ComponentFactoryResolver, ViewContainerRef, ChangeDetectorRef, TemplateRef, ViewChild, Output, EventEmitter } from '@angular/core';
+import {AlertsComponent} from './alerts/alerts.component';
+import {BrowserModule} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -7,10 +8,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-
+  
   title = 'plussa-gui';
+  @ViewChild("alertContainer", { read: ViewContainerRef }) container;
+  constructor(private resolver: ComponentFactoryResolver) {}
 
-  constructor() { } 
+  createComponent(text) {
+    this.container.clear(); 
+    const factory: ComponentFactory = this.resolver.resolveComponentFactory(AlertsComponent);
+    this.componentRef: ComponentRef = this.container.createComponent(factory);
+  }
 
 
 
